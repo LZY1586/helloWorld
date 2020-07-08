4ee750149897d7f02d53ff4de0e8d19ee154cc17
e <1586254416@qq.com>
2020-07-08 17:58:21 +0800

    dsa

---
+++ 学习笔记.c
@@ -0,0 +1,69 @@
+CTRL+D  注释
+CTRL+E  解除注释
+
+打印的三种方式：
+log message  输出日志
+Write line   输出一行
+message box  弹窗输出
+
+变量-在活动之间传递数据
+assign  赋值控件
+
+参数-在流程之间传递数据
+in      输入
+out     输出
+in/out  输入/输出
+
+鼠标操作
+click         单击
+double click  双击
+hover         悬停
+click text    点击文本
+
+键盘操作
+send hotkey  发送热键
+type into     输入信息
+set to clipboard    设置剪贴板
+get from clipboard  从剪切板获取
+get text      获取输入框内字符
+
+文本操作：
+set text  设置文本
+get text  获取文本
+get full text 获取全文本
+get ocr text  获取OCR文本
+get visibletext  获取可见文本
+
+text exists  存在文本
+uielement exists  存在元素
+image exists  存在图片
+
+图片操作：
+take screenshot  截图
+saveimage  保存截图
+
+workbook  表格操作
+output data table  将表格转化为字符串
+
+delay  延时
+maximize window  窗口最大化
+attach window    指定一个窗口
+open browser     打开浏览器
+contains  判断是否包含某个字符
+
+条件判断
+if     通过序列
+flow desicion   通过流程
+
+循环
+for each
+do while
+while
+break
+continue
+
+
+select folder  获取文件路径
+
+
+
